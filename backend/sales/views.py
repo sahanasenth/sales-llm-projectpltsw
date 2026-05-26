df9e2923f25eadf05fda15bdf815589376e36287
krishmitha2607 <dkrishmitha@gmail.com>
2026-05-26 17:09:58 +0530

    Import permission classes for API views
    Added permission classes for API views.

--- backend/sales/views.py
+++ backend/sales/views.py
@@ -1,3 +1,18 @@
+from rest_framework.decorators import (
+    api_view,
+    permission_classes
+)
+
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny
+)
+
+from .permissions import (
+    IsDirector,
+    IsSalesManager,
+    IsCRMUser
+)
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
